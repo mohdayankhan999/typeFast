acb290995c863367b76404836642684cbee47cd1
mr.knight <mohdayankhan999@gmail.com>
2022-04-19 20:03:28 +0530

    Automati configuration with django-heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import imp
 from pathlib import Path
 import os
 
@@ -125,5 +126,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku 
+django_heroku.settings(locals())
 
 
